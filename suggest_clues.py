47e2b92476d3588551eea08688564206c67171e0
Sebastien Chiasson <sjchiass@gmail.com>
2025-03-23 16:55:42 -0400

    Create clue generator

---
+++ suggest_clues.py
@@ -0,0 +1,69 @@
+from utilities.llm import *
+from utilities.corpus import *
+import argparse
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(
+    prog="",
+    description="T",
+)
+
+parser.add_argument(
+    "input",
+    type=str,
+    help="The input JSON file.",
+)
+
+args = parser.parse_args()
+
+corpus = corpus_from_json(open(args.input, "r"))
+
+for word in (pbar := tqdm(corpus.vocabulary)):
+    pbar.set_description(f"Summarizing {word.word}")
+    llm = LLM()
+    if word.summary is None:
+        doc = word.doc
+        # There seems to be a limit of input that Ollama can take
+        # The Ollama server will crash if it gets too much
+        generate = llm.summarize(doc[:1000])
+        word.summary = generate
+        # Save our work
+        corpus.to_json(open(args.input, "w"))
+        # Try to prompt the LLM more
+        llm.gen("Refer to your summary's information when answering my next question.")
+        print(word.word, "\n---")
+        print(word.summary + "\n")
+        while True:
+            clue = llm.give_clue(word.word + " " + word.category)
+            user_input = input(
+                "\n" + clue + "\nDo you accept this clue? Y/n or E to edit > "
+            ).lower()
+            if user_input in ["y", ""]:
+                break
+            elif user_input == "e":
+                clue = input("Write your own clue and press ENTER to save. > ")
+                break
+        word.clue_en = clue
+    elif word.clue_en is None:
+        llm.gen(
+            "Read the following:\n\n"
+            + word.summary
+            + "\n\nRefer to its information when answering my next question."
+        )
+        print(word.word, "\n---")
+        print(word.summary + "\n")
+        while True:
+            clue = llm.give_clue(word.word + " " + word.category)
+            user_input = input(
+                "\n" + clue + "\nDo you accept this clue? Y/n or E to edit > "
+            ).lower()
+            if user_input in ["y", ""]:
+                break
+            elif user_input == "e":
+                clue = input("Write your own clue and press ENTER to save. > ")
+                break
+        word.clue_en = clue
+    # save
+    corpus.to_json(open(args.input, "w"))
+
+corpus.to_json(open(args.input, "w"))
